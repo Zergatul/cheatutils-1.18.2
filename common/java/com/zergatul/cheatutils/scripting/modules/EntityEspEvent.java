3cbbdf1a0552447c9d55afbe43202a6c2e184bac
Igor Budzhak <z3rgatul@gmail.com>
2024-11-16 23:54:09 +0200

    Entity ESP scripting fix for edge cases

--- common/java/com/zergatul/cheatutils/scripting/modules/EntityEspEvent.java
+++ common/java/com/zergatul/cheatutils/scripting/modules/EntityEspEvent.java
@@ -10,43 +10,45 @@ import net.minecraft.network.chat.Style;
 @SuppressWarnings("unused")
 public class EntityEspEvent {
 
-    public static final EntityEspEvent instance = new EntityEspEvent();
+    private final EntityEsp.EntityScriptResult result;
 
-    private EntityEspEvent() {}
+    public EntityEspEvent(EntityEsp.EntityScriptResult result) {
+        this.result = result;
+    }
 
     @MethodDescription("""
             Disables tracer for current entity
             """)
     public void disableTracer() {
-        EntityEsp.EntityScriptResult.current.tracerDisabled = true;
+        result.tracerDisabled = true;
     }
 
     @MethodDescription("""
             Disables outline for current entity
             """)
     public void disableOutline() {
-        EntityEsp.EntityScriptResult.current.outlineDisabled = true;
+        result.outlineDisabled = true;
     }
 
     @MethodDescription("""
             Disables overlay for current entity
             """)
     public void disableOverlay() {
-        EntityEsp.EntityScriptResult.current.overlayDisabled = true;
+        result.overlayDisabled = true;
     }
 
     @MethodDescription("""
             Disables collision box for current entity
             """)
     public void disableCollisionBox() {
-        EntityEsp.EntityScriptResult.current.collisionBoxDisabled = true;
+        result.collisionBoxDisabled = true;
     }
 
     @MethodDescription("""
             Overrides title displayed above the entity. Works only with cheatutils title system
             """)
     public void setTitle(String title) {
-        EntityEsp.EntityScriptResult.current.title = StylizedText.of(title);
+        result.title = StylizedText.of(title);
     }
 
     @MethodDescription("""
@@ -67,6 +69,6 @@ public class EntityEspEvent {
             text.chunks.add(new StylizedTextChunk(data[i + 1], Style.EMPTY.withColor(color)));
         }
 
-        EntityEsp.EntityScriptResult.current.title = text;
+        result.title = text;
     }
 }
