346bd5db194a169c541bbdd5b35593c5a316635d
Igor Budzhak <z3rgatul@gmail.com>
2024-12-12 20:16:45 +0200

    Auto Fish: added auto restart

--- common/java/com/zergatul/cheatutils/configs/AutoFishConfig.java
+++ common/java/com/zergatul/cheatutils/configs/AutoFishConfig.java
@@ -1,3 +1,18 @@
 package com.zergatul.cheatutils.configs;
 
-public class AutoFishConfig extends ModuleConfig {}
+import com.zergatul.cheatutils.utils.MathUtils;
+
+public class AutoFishConfig extends ModuleConfig implements ValidatableConfig {
+
+    public boolean autoRestartOnIdle;
+    public int idleTimeout;
+
+    public AutoFishConfig() {
+        idleTimeout = 60;
+    }
+
+    @Override
+    public void validate() {
+        idleTimeout = MathUtils.clamp(idleTimeout, 5, 3600);
+    }
+}
