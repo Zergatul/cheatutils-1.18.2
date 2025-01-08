598e160230545a762bbbe4051c29f2b22aa9c16e
Igor Budzhak <z3rgatul@gmail.com>
2025-01-08 12:59:11 +0200

    armor overlay fixed missing durability

--- common/java/com/zergatul/cheatutils/modules/visuals/ArmorOverlay.java
+++ common/java/com/zergatul/cheatutils/modules/visuals/ArmorOverlay.java
@@ -1,5 +1,6 @@
 package com.zergatul.cheatutils.modules.visuals;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -10,18 +11,25 @@ public class ArmorOverlay {
 
     public static final ArmorOverlay instance = new ArmorOverlay();
 
+    private final Minecraft mc = Minecraft.getInstance();
+
     private ArmorOverlay() {
 
     }
 
     public void render(GuiGraphics graphics, Player player, int left, int top) {
         List<ItemStack> armor = player.getInventory().armor;
-        graphics.renderItem(armor.get(3), left, top);
+        renderItem(graphics, armor.get(3), left, top);
         left += 16;
-        graphics.renderItem(armor.get(2), left, top);
+        renderItem(graphics, armor.get(2), left, top);
         left += 16;
-        graphics.renderItem(armor.get(1), left, top);
+        renderItem(graphics, armor.get(1), left, top);
         left += 16;
-        graphics.renderItem(armor.get(0), left, top);
+        renderItem(graphics, armor.get(0), left, top);
+    }
+
+    private void renderItem(GuiGraphics graphics, ItemStack itemStack, int left, int top) {
+        graphics.renderItem(itemStack, left, top);
+        graphics.renderItemDecorations(mc.font, itemStack, left, top);
     }
 }
